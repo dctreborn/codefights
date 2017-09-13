b9b5876eb904d9c66791d9dc77ba412ae7d7acec
Rukaroa <rukaroa@gmail.com>
2017-09-13 18:54:20 -0500

    problem 17 done

---
+++ Intro/17_arrayChange.js
@@ -0,0 +1,36 @@
+// You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
+// Example
+
+// For inputArray = [1, 1, 1], the output should be
+// arrayChange(inputArray) = 3.
+
+// Input/Output
+
+// [time limit] 4000ms (js)
+// [input] array.integer inputArray
+
+// Guaranteed constraints:
+// 3 ≤ inputArray.length ≤ 105,
+// -105 ≤ inputArray[i] ≤ 105.
+
+// [output] integer
+
+// The minimal number of moves needed to obtain a strictly increasing sequence from inputArray.
+// It's guaranteed that for the given test cases the answer always fits signed 32-bit integer type.
+
+function arrayChange(inputArray) {
+    let moves = 0;
+    let temp = inputArray;
+    let size = temp.length;
+    
+    for (var i = 0; i < size - 1; i++){        
+        if (temp[i] >= temp[i+1]){
+            let diff = temp[i] - temp[i+1] + 1;
+            moves += diff;
+            temp[i+1] += diff;
+        }
+    }
+    
+    return moves;
+}
