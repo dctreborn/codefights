83664df933797e3382f78353cf1ab6238e4dbf1a
Rukaroa <rukaroa@gmail.com>
2017-09-30 08:52:08 -0500

    fiboccani root reverse challenge

---
+++ Challenges/fibFib.js
@@ -0,0 +1,30 @@
+// This is a reverse challenge, enjoy!
+
+// Input/Output
+
+//     [time limit] 4000ms (js)
+
+//     [input] integer ff
+
+//     Guaranteed constraints:
+//     0 ≤ ff ≤ 108.
+
+//     [output] boolean
+
+// Challenge: is the square root of the given number a fibonacci number?
+
+function fibFib(ff) {
+    let root = Math.sqrt(ff);
+    let a = 0, b = 1, temp;
+    
+    while(a <= root){
+        if (root == a || root == b) {
+            return true;
+        }
+        temp = a;
+        a += b;
+        b = temp;
+    }
+    
+    return false
+}
