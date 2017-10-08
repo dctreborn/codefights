d189686e040438e130c1b90356902b8a98890446
Rukaroa <rukaroa@gmail.com>
2017-10-08 14:53:17 -0500

    truncating string challenge. that was easy.

---
+++ Challenges/truncateString.js
@@ -0,0 +1,48 @@
+// Consider the following operation on a string containing digits from 1 to 9:
+
+//     if the leftmost digit of the string is divisible by 3, remove it from the string;
+//     otherwise, if the rightmost digit of the string is divisible by 3, remove it from the string;
+//     otherwise, if the sum of two digits on the sides of the string is divisible by 3, remove both digits from the string;
+
+// This operation is applied sequentially until the string is empty or neither of the three given conditions is met.
+
+// For a given string find the result of applying the given algorithm to it.
+
+// Example
+
+// For s = "312248", the output should be
+// truncateString(s) = "2".
+
+// Here's how the answer is obtained:
+
+//     3 is divisible by 3 and should be removed ("12248");
+//     neither 1 nor 8 is divisible by 3, but their sum is, so both digits should be removed ("224");
+//     neither 2 nor 4 is divisible by 3, but their sum is, so both digits should be removed ("2");
+//     the resulting string "2" doesn't meet any condition, so it is the final answer.
+
+// Input/Output
+
+//     [time limit] 4000ms (js)
+
+//     [input] string s
+
+//     Guaranteed constraints:
+//     1 ≤ s.length ≤ 15.
+
+//     [output] string
+
+function truncateString(s) {
+    let a = s[0], len = s.length - 1, b = s[len], t = false, z;
+    
+    z = a % 3 == 0 ? s.substring(1) :
+        b % 3 == 0 ? s.substring(0, len) :
+        (a + b) % 3 == 0 ? s.substring(1, len) : s;
+    
+    t = z == s ? true : false;
+    
+    if (t) {
+        return z;
+    } else {
+        return truncateString(z);
+    }
+}
