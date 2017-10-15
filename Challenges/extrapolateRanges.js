3fe2e91df09ab74c5e16cd02cc47fedac214a97c
Rukaroa <rukaroa@gmail.com>
2017-10-15 15:27:07 -0500

    adjusted but does not pass all tests. to revisit.

--- Challenges/extrapolateRanges.js
+++ Challenges/extrapolateRanges.js
@@ -20,28 +20,32 @@
 //     A string of comma delimited ranges or "undefined". A range can be a single integer, or an inclusive list of integers specified in the following format: {min}-{max} (e.g. 5-9).
 
 function extrapolateRanges(str) {
-    let s = str.split(","), num1, num2, output = "", end, min;
-    s.sort( (a, b) => parseInt(a) - parseInt(b));
-    console.log(s);
-    for(let i = 0; i < s.length; i++){
-        num1 = parseInt(s[i]);
-        end = output == null ? "" : output.charAt(output.length-1);
+    function extrapolateRanges(str) {
+        let s = str.split(","), num1, num2, output = "", end, min;
+        s.sort( (a, b) => parseInt(a) - parseInt(b));
+        console.log(s);
+        for(let i = 0; i < s.length; i++){
+            num1 = parseInt(s[i]);
+            end = output == null ? "" : output.charAt(output.length-1);
+            
+            if(num1 < 1){
+                return "undefined";
+            } else if (i == 0){
+                min = num1;
+            } else if (num1 == num2){
+                //do nothing
+            } else if (num1 > num2 + 1 && num2 == min){
+                output += (min + ",");
+                min = num1;
+            } else if (num1 > num2 + 1){
+                output += (min + "-" + num2 + ",");
+                min = num1;
+            } else if (i == s.length - 1){
+                output += (min + "-");
+            }
+            
+            num2 = num1;
+        }
         
-        if(num1 < 1){
-            return "undefined";
-        } else if (i == 0){
-            min = num1;
-        } else if (num1 == num2){
-            //do nothing?
-            min = num1;
-        }    
-        else if (num1 > num2 + 1 && num1 != num2){
-            output += (min + "-" + num2 + ",");
-            min = num1;
-        } 
-        
-        num2 = num1;
+        return output + num2;
     }
-    
-    return output + num2;
-}
